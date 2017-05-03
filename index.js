7cad7fa2cee03e81182838c1c3158deddd18ead0
Joel <joel@tractable.io>
2017-05-03 13:47:11 +0100

    Updates tmpName to use Math.random() ..
    .. in stead of external UUID package

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const Busboy = require('busboy');
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
-const uuidV4 = require('uuid/v4');
 
 const getDescriptor = Object.getOwnPropertyDescriptor;
 
@@ -94,7 +93,7 @@ function onField(fields, name, val, fieldnameTruncated, valTruncated) {
 }
 
 function onFile(filePromises, fieldname, file, filename, encoding, mimetype) {
-  const tmpName = file.tmpName = uuidV4() + '-' + filename;
+  const tmpName = file.tmpName = Math.random().toString(16).substring(2) + '-' + filename;
   const saveTo = path.join(os.tmpdir(), path.basename(tmpName));
   const writeStream = fs.createWriteStream(saveTo);
   const filePromise = new Promise((resolve, reject) => writeStream
